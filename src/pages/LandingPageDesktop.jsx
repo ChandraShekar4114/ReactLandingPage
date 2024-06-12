32218351722d95b6cc8b8bcfb8998c7e2ee9333a
GV Chandra Shekar <137907463+ChandraShekar4114@users.noreply.github.com>
2024-06-12 22:22:44 +0530

    Add files via upload

---
+++ src/pages/LandingPageDesktop.jsx
@@ -0,0 +1,286 @@
+import "./LandingPageDesktop.css";
+
+const LandingPageDesktop = () => {
+  return (
+    <div className="landing-page-desktop">
+      <div className="landing-page-desktop-child" />
+      <div className="designers-and-makers-parent">
+        <div className="designers-and-makers">Designers and makers</div>
+        <div className="discover-the-artistry">
+          Discover the artistry and craftsmanship behind HomeVibe with our
+          skilled designers and makers
+        </div>
+        <div className="zangmo">Zangmo</div>
+        <img
+          className="pexels-ono-kosuki-5973966-1-icon"
+          alt=""
+          src="/pexelsonokosuki5973966-1@2x.png"
+        />
+        <div className="secondary-button-small">
+          <div className="secondary-button">Know more</div>
+        </div>
+      </div>
+      <img className="hero-image-icon" alt="" src="/hero-image@2x.png" />
+      <div className="header">
+        <div className="homevibes">
+          <div className="homevibes1">HomeVibes</div>
+        </div>
+        <div className="leading-icon-parent">
+          <img className="leading-icon" alt="" src="/leading-icon.svg" />
+          <img className="leading-icon" alt="" src="/leading-icon1.svg" />
+          <img className="leading-icon" alt="" src="/leading-icon2.svg" />
+        </div>
+        <img className="leading-icon3" alt="" src="/leading-icon3.svg" />
+        <div className="log-in-cta">
+          <img className="leading-icon" alt="" src="/leading-icon4.svg" />
+          <div className="secondary-button">Log in/ Sign up</div>
+        </div>
+      </div>
+      <div className="primary-button-small">
+        <div className="secondary-button">View our latest collection</div>
+      </div>
+      <div className="crafted-with-passion">
+        Crafted with passion, designed for comfort
+      </div>
+      <div className="secondary-button-small1">
+        <div className="secondary-button">Browse categories</div>
+      </div>
+      <div className="sofa-1">
+        <img className="sofa-1-icon" alt="" src="/sofa-1@2x.png" />
+      </div>
+      <div className="categories">Categories</div>
+      <div className="section-2">
+        <div className="new-arrivals">
+          <div className="new-arrivals1">New arrivals</div>
+        </div>
+        <div className="best-sellers">
+          <div className="best-sellers1">Best sellers</div>
+          <img className="best-sellers-child" alt="" src="/ellipse-1@2x.png" />
+          <img className="plant-1-icon" alt="" src="/plant-1@2x.png" />
+          <img
+            className="pexels-eneida-nieves-1112598-1-icon"
+            alt=""
+            src="/pexelseneidanieves1112598-1@2x.png"
+          />
+          <img
+            className="lamp-thai-style-1-icon"
+            alt=""
+            src="/lampthaistyle-1@2x.png"
+          />
+          <img
+            className="pexels-mikhail-nilov-6707632-r-icon"
+            alt=""
+            src="/pexelsmikhailnilov6707632removebgpreview-1@2x.png"
+          />
+        </div>
+      </div>
+      <div className="category-card">
+        <div className="furniture">Furniture</div>
+        <div className="image">
+          <div className="image1" />
+        </div>
+        <div className="image2">
+          <div className="image1" />
+        </div>
+        <div className="image4">
+          <div className="image1" />
+        </div>
+        <div className="image6">
+          <div className="image1" />
+        </div>
+        <div className="image8">
+          <div className="image1" />
+        </div>
+      </div>
+      <div className="category-card1">
+        <div className="furniture">{`Décor `}</div>
+        <div className="image10">
+          <div className="image1" />
+        </div>
+        <div className="image12">
+          <div className="image1" />
+        </div>
+        <div className="image14">
+          <div className="image1" />
+        </div>
+        <div className="image16">
+          <div className="image1" />
+        </div>
+        <div className="image18">
+          <div className="image1" />
+        </div>
+      </div>
+      <div className="category-card2">
+        <div className="furniture">LIGHTING</div>
+        <div className="image20">
+          <div className="image1" />
+        </div>
+        <div className="image22">
+          <div className="image1" />
+        </div>
+        <div className="image24">
+          <div className="image1" />
+        </div>
+        <div className="image26">
+          <div className="image1" />
+        </div>
+        <div className="image28">
+          <div className="image1" />
+        </div>
+      </div>
+      <div className="exciting-collections">Exciting collections</div>
+      <div className="curated-specially-for">curated specially for you</div>
+      <div className="testimonials-44">
+        <div className="frame-parent">
+          <div className="frame-group">
+            <div className="icon-pack-parent">
+              <img className="leading-icon" alt="" src="/icon-pack.svg" />
+              <div className="it-is-a-long-established-fact-wrapper">
+                <div className="it-is-a">
+                  I purchased a set of outdoor furniture from this company and I
+                  am so impressed with the durability and design. The furniture
+                  has held up well in all types of weather and the cushions are
+                  easy to clean.
+                </div>
+              </div>
+            </div>
+            <div className="avatar-parent">
+              <img className="avatar-icon" alt="" src="/avatar@2x.png" />
+              <div className="lora-smith">{`Lora Smith `}</div>
+            </div>
+          </div>
+          <div className="frame-container">
+            <div className="icon-pack-parent">
+              <img className="leading-icon" alt="" src="/icon-pack.svg" />
+              <div className="it-is-a-long-established-fact-wrapper">
+                <div className="it-is-a">
+                  "I purchased a bed frame from this furniture brand and it
+                  exceeded my expectations. The quality is top-notch and the
+                  design is modern and sleek. The customer service was also
+                  fantastic, and the delivery was prompt. I highly recommend
+                  this furniture brand
+                </div>
+              </div>
+            </div>
+            <div className="avatar-parent">
+              <img className="avatar-icon" alt="" src="/avatar@2x.png" />
+              <div className="lora-smith">{`Lora Smith `}</div>
+            </div>
+          </div>
+          <div className="frame-div">
+            <div className="icon-pack-container">
+              <img className="leading-icon" alt="" src="/icon-pack.svg" />
+              <div className="it-is-a-long-established-fact-wrapper">
+                <div className="it-is-a2">
+                  I recently purchased a sectional sofa from this company and I
+                  have been extremely pleased with it. The fabric is high
+                  quality and the cushions are very comfortable. The delivery
+                  process was smooth and hassle-free. I highly recommend
+                  HomeVibes
+                </div>
+              </div>
+            </div>
+            <div className="avatar-container">
+              <img className="avatar-icon" alt="" src="/avatar@2x.png" />
+              <div className="lora-smith">{`Lora Smith `}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+        <div className="terms-conditions-parent">
+          <div className="terms-conditions">{`TERMS & COnditions`}</div>
+          <div className="privacy-policy">privacy policy</div>
+        </div>
+        <div className="homevibes-parent">
+          <div className="homevibes2">
+            <div className="homevibes3">
+              <span>Home</span>
+              <span className="vibes">Vibes</span>
+            </div>
+          </div>
+          <img className="logo-icon" alt="" src="/logo.svg" />
+        </div>
+        <div className="keep-up-to-date-with-new-colle-parent">
+          <div className="keep-up-to">
+            Keep up to date with new collection and festive sales
+          </div>
+          <div className="enter-your-email">Enter your email</div>
+          <div className="we-respect-your-container">
+            <span className="we-respect-your-container1">
+              <span>{`We respect your privacy, we’ll not spam you ! Read our `}</span>
+              <span className="privacy-policy1">privacy policy</span>
+              <span> here.</span>
+            </span>
+          </div>
+          <div className="group-child" />
+          <div className="primary-button-small1">
+            <div className="primary-button2">Subscribe</div>
+          </div>
+        </div>
+        <div className="group-div">
+          <div className="about-us-parent">
+            <div className="about-us">About us</div>
+            <div className="about-us">Careers</div>
+            <div className="about-us">Contact us</div>
+            <div className="find-a-showroom">Find a showroom</div>
+          </div>
+          <div className="company">COmpany</div>
+        </div>
+        <div className="frame-parent1">
+          <div className="categories-parent">
+            <div className="about-us">Categories</div>
+            <div className="about-us">New Arrivals</div>
+            <div className="about-us">Best sellers</div>
+            <div className="find-a-showroom">Collections</div>
+          </div>
+          <div className="product">PRODUCT</div>
+        </div>
+      </div>
+      <img className="collections-icon" alt="" src="/collections@2x.png" />
+      <img className="collections-icon1" alt="" src="/collections@2x.png" />
+      <img className="collections-icon2" alt="" src="/collections@2x.png" />
+      <div className="contact-us-parent">
+        <div className="contact-us1">Contact us</div>
+        <div className="full-name-parent">
+          <div className="full-name">Full Name</div>
+          <img className="instance-child" alt="" src="/vector-1.svg" />
+        </div>
+        <div className="full-name-group">
+          <div className="full-name">E-mail</div>
+          <img className="instance-child" alt="" src="/vector-1.svg" />
+        </div>
+        <div className="full-name-container">
+          <div className="full-name">Message</div>
+          <img className="instance-child" alt="" src="/vector-1.svg" />
+        </div>
+        <div className="primary-button-small2">
+          <div className="secondary-button">Contact us</div>
+        </div>
+        <div className="leading-icon-group">
+          <img className="leading-icon5" alt="" src="/leading-icon5.svg" />
+          <div className="email-us">Email us @ xyz@HomeVibes.com</div>
+        </div>
+        <div className="facebook-negative-parent">
+          <img
+            className="facebook-negative"
+            alt=""
+            src="/facebook--negative.svg"
+          />
+          <img
+            className="facebook-negative"
+            alt=""
+            src="/instagram--negative.svg"
+          />
+          <img
+            className="facebook-negative"
+            alt=""
+            src="/twitter--negative.svg"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPageDesktop;
